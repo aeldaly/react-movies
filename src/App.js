b59b87d9de1d875edad940417f9f85df2e562127
Ahmed El-Daly <aeldaly@gmail.com>
2017-10-22 18:16:46 -0400

    WIP = redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 import Grid from './components/Grid.jsx';
 import Single from './components/Single.jsx';
 
+import store from './store.jsx';
+
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
+
+import createHistory from 'history/createBrowserHistory'
+
+const history = createHistory();
+
+
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <ConnectedRouter history={history}>
         <div className="App">
           <Route exact path='/' component={Grid} />
           <Route path='/view/:id' component={Single} />
         </div>
-      </BrowserRouter>
+      </ConnectedRouter>
     );
   }
 }
